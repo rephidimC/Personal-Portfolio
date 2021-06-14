c8116b0e2da16190a9f404b9120c682de06e8232
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-06-14 14:42:36 +0100

    completed homepage for mobile

--- my-project/pages/_app.js
+++ my-project/pages/_app.js
@@ -1,4 +1,12 @@
 import 'tailwindcss/tailwind.css'
+// import Link from 'next/link'
+// <Link href="/posts/xxx-post">
+//   <a>this page</a>
+// </Link>
+
+
+
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
